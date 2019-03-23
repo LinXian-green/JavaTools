c5e9dc13f5b0d6ae7088f9203543047df7eb0972
LinXian-green <707574695@qq.com>
2019-03-23 10:34:57 +0800

    数据库连接池工具类第一次提交

---
+++ src/main/java/DataSourceConnectionPool/ConnectionPoolTest.java
@@ -0,0 +1,18 @@
+package DataSourceConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectionPoolTest {
+    public static void main(String[] args){
+        String sql = " select id from user";
+        try {
+            ConnectionPool pool = ConnectionPool.getInstance();
+            Connection conn = pool.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
